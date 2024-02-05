fdb66c6a6440647954924828d47b4adeb46d3d92
Muhammad Adjie Bimaditya <madjiebimaa@gmail.com>
2024-02-05 20:43:00 +0700

    feat: add product search component

---
+++ src/components/product/product-search.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { Search } from "lucide-react"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
+import { useDebouncedCallback } from "use-debounce"
+
+import { Input } from "@/components/ui/input"
+
+export default function ProductSearch() {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const router = useRouter()
+
+  const createQuery = useDebouncedCallback((query: string) => {
+    const params = new URLSearchParams(searchParams)
+    query ? params.set("query", query) : params.delete("query")
+    router.replace(`${pathname}?${params.toString()}`)
+  }, 300)
+
+  return (
+    <div className="relative">
+      <Search className="absolute top-2 left-2 shrink-0 h-6 w-6 text-slate-400" />
+      <Input
+        type="text"
+        placeholder="Search..."
+        autoComplete="off"
+        className="pl-10"
+        onChange={(event) => createQuery(event.target.value)}
+      />
+    </div>
+  )
+}
