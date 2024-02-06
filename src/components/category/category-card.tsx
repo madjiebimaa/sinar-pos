85d580fc9a3e0052d4eba7aa9ba6c2f5e9cf4740
Muhammad Adjie Bimaditya <madjiebimaa@gmail.com>
2024-02-06 12:11:02 +0700

    feat: add total items element to the category card

--- src/components/category/category-card.tsx
+++ src/components/category/category-card.tsx
@@ -1,21 +1,33 @@
+"use client"
+
+import { useMemo } from "react"
+
 import { Card, CardContent } from "@/components/ui/card"
 
 import { Category } from "@/lib/types"
-import { cn, getCategoryCardStyle } from "@/lib/utils"
+import { cn, getCategoryStyle, getTotalCategoryItems } from "@/lib/utils"
+import { useOrder } from "@/store/order"
 
 interface CategoryCardProps {
   category: Category
 }
 
 export default function CategoryCard({ category }: CategoryCardProps) {
-  const { icon: Icon, backgroundColor } = getCategoryCardStyle(category.name)
+  const order = useOrder()
+
+  const totalItems = useMemo(
+    () => getTotalCategoryItems(order.items, category.name),
+    [category.name, order.items]
+  )
+  const { icon: Icon, backgroundColor } = getCategoryStyle(category.name)
 
   return (
     <Card className={cn("border-none", backgroundColor)}>
-      <CardContent className="flex flex-col justify-between h-[120px] w-[150px] p-4">
+      <CardContent className="flex flex-col justify-between h-[130px] w-[130px] p-4">
         <Icon className="shrink-0 h-4 w-4" />
-        <div className="flex flex-col">
+        <div className="flex flex-col space-y-1">
           <p className="font-medium text-sm">{category.name}</p>
+          <span className="text-xs text-slate-600">{totalItems} items</span>
         </div>
       </CardContent>
     </Card>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,23 +2,73 @@ import { clsx, type ClassValue } from "clsx"
 import { Bolt, Cuboid, Droplet, InspectionPanel, Zap } from "lucide-react"
 import { twMerge } from "tailwind-merge"
 
-import { Category, CategoryStyle } from "@/lib/types"
+import { Category, CategoryStyle, Order } from "@/lib/types"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 const categories: Map<Category["name"], CategoryStyle> = new Map([
-  ["Concrete and Masonry", { icon: Cuboid, backgroundColor: "bg-green-100" }],
-  ["Fasteners and Hardware", { icon: Bolt, backgroundColor: "bg-cyan-100" }],
+  [
+    "Concrete and Masonry",
+    {
+      icon: Cuboid,
+      textColor: "text-green-100",
+      backgroundColor: "bg-green-100",
+      borderColor: "border-green-100",
+    },
+  ],
+  [
+    "Fasteners and Hardware",
+    {
+      icon: Bolt,
+      textColor: "text-cyan-100",
+      backgroundColor: "bg-cyan-100",
+      borderColor: "border-cyan-100",
+    },
+  ],
   [
     "Flooring Materials",
-    { icon: InspectionPanel, backgroundColor: "bg-yellow-100" },
+    {
+      icon: InspectionPanel,
+      textColor: "text-yellow-100",
+      backgroundColor: "bg-yellow-100",
+      borderColor: "border-yellow-100",
+    },
   ],
-  ["Plumbing Materials", { icon: Droplet, backgroundColor: "bg-blue-100" }],
-  ["Electrical Supplies", { icon: Zap, backgroundColor: "bg-red-100" }],
+  [
+    "Plumbing Materials",
+    {
+      icon: Droplet,
+      textColor: "text-blue-100",
+      backgroundColor: "bg-blue-100",
+      borderColor: "border-blue-100",
+    },
+  ],
+  [
+    "Electrical Supplies",
+    {
+      icon: Zap,
+      textColor: "text-red-100",
+      backgroundColor: "bg-red-100",
+      borderColor: "border-red-100",
+    },
+  ],
 ])
 
-export function getCategoryCardStyle(name: Category["name"]): CategoryStyle {
+export function getCategoryStyle(name: Category["name"]): CategoryStyle {
   return categories.get(name)!
+}
+
+export function getTotalCategoryItems(
+  items: Order["items"],
+  name: Category["name"]
+): number {
+  return items.reduce((accumulator, item) => {
+    if (item.category.name === name) {
+      return accumulator + item.quantity
+    }
+
+    return accumulator
+  }, 0)
 }
